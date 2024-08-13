bfdc0c519236dd100ed9cadfbf4278636a40c19c
UserMMT <69217959+UserMMT@users.noreply.github.com>
2024-08-13 00:18:04 +0000

    save local & fix pdf.pipe

--- index.js
+++ index.js
@@ -3,7 +3,37 @@ require('dotenv').config();
 const { time } = require('console');
 const http = require('http');
 const puppeteer = require('puppeteer');
+const fs = require("fs")
 
+// import {Readable} from 'stream';
+var browser=null;
+const widthGF= Number(process.env.GRAFANA_PDF_WIDTH || 1920) ;
+// console.log(widthGF);
+
+async function initBrowser(options) {
+  const {
+    backendUser,
+    backendPass,
+    backendApiKey,
+    timeoutDuration,
+    backendNoLogin,
+    backendPdfSaveLocal,
+    executablePath,
+    width = 1920,
+    height = parseInt(1200 * Math.sqrt(2)),
+  } = options;
+
+  console.log('Launching chromium:', executablePath || '(puppeteer builtin)');
+  browser = await puppeteer.launch({
+    executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+    args: ["--no-sandbox",
+        "--disable-setuid-sandbox","--disable-dev-shm-usage"],
+    timeout:0
+  });
+  const pageDefault = await browser.newPage();
+}
 /**
  * Create a readable PDF stream by fetching the given URL with puppeteer.
  *
@@ -15,21 +45,25 @@ async function streamPdf(url, options) {
   const {
     backendUser,
     backendPass,
+    backendApiKey,
     timeoutDuration,
     backendNoLogin,
     backendPdfSaveLocal,
     executablePath,
-    width = 1200,
+    width =widthGF,
     height = parseInt(1200 * Math.sqrt(2)),
   } = options;
-
+/*
   console.log('Launching chromium:', executablePath || '(puppeteer builtin)');
   const browser = await puppeteer.launch({
     executablePath,
     headless: true,
+    ignoreHTTPSErrors: true,
+    args: ["--no-sandbox",
+        "--disable-setuid-sandbox","--disable-dev-shm-usage"],
     timeout:0
   });
-
+*/
   const page = await browser.newPage();
   if (timeoutDuration) {
     page.setDefaultNavigationTimeout(1000*timeoutDuration)
@@ -44,7 +78,10 @@ async function streamPdf(url, options) {
 
   console.log('Fetching url');
   if (backendNoLogin==0){
-  if (backendUser && backendPass) {
+  if (backendApiKey) {
+    await page.setExtraHTTPHeaders({ 'Authorization': "Bearer "+ backendApiKey  });  
+    }
+  else if (backendUser && backendPass) {
     const authHeader = 'Basic ' + new Buffer.from(`${backendUser}:${backendPass}`).toString('base64');
     await page.setExtraHTTPHeaders({ 'Authorization': authHeader });
   }
@@ -61,7 +98,16 @@ async function streamPdf(url, options) {
     const resizeHandles = document.getElementsByClassName('react-resizable-handle');
     for (el of resizeHandles) { el.hidden = true; };
 
+  }); 
+  
+  // var dashboard_name = getTitle();
+ var dashboard_name = await page.evaluate(() => {
+  // let title_page=document.getElementsByClassName('css-1spogwh')[0] 
+  //      return title_page.children[title_page.childElementCount -1].innerText 
+  return document.title.split('-')[0]
   });
+      console.log(dashboard_name);
+     dashboard_name=dashboard_name.replaceAll(' ','_');
 	// Get the height of the main canvas, and add a margin
      var height_px = await page.evaluate(() => {
       return document.getElementsByClassName('react-grid-layout')[0].getBoundingClientRect().bottom;
@@ -105,28 +151,38 @@ async function streamPdf(url, options) {
 
 
   console.log('Rendering PDF');
-  return (await page.createPDFStream({
+  // var pdfOutput = (await page.createPDFStream({
+  const fileLocation = 'tmp/'+ ((dashboard_name+'__'+dateFormat(new Date())+'.pdf').replaceAll("/","_")) ;
+  (await page.pdf({
     width: width +'px',
     height: height_px+'px',
-    scale: 1,
-    format: 'tabloid',
+    // scale: 1,
+    // format: 'tabloid',
     displayHeaderFooter: false,
-    margin: {
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0,
-    },
-    // landscape:true,
-    path : 'test.pdf',
+    // margin: {
+    //   top: 0,
+    //   right: 0,
+    //   bottom: 0,
+    //   left: 0,
+    // },
+    landscape:true,
+    path : fileLocation,
     printBackground: true,
-  })).on('close', () => {
-    console.log('Finished rendering PDF');
-    browser.close();
-    console.log('Terminated chromium');
-  });
+  }))
+  
+  console.log('Finished rendering PDF '+fileLocation);
+  page.close();
+  return (fileLocation);
+  /*
+  await  browser.close();
+  console.log('Terminated chromium');
+  return (pdfOutput);
+  */
 }
-
+function getTitle() {
+  let title_page=document.getElementsByClassName('css-1spogwh')[0] 
+  return title_page.children[title_page.childElementCount -1].innerText 
+}
 function startServer(options) {
   const {
     port,
@@ -134,42 +190,70 @@ function startServer(options) {
     backendUrl,
     backendUser,
     backendPass,
+    backendApiKey,
     timeoutDuration,
     backendPdfSaveLocal,
     backendNoLogin,
     executablePath
   } = options;
-
+    
   return http.createServer(async (request, response) => {
-    const kioskParam = request.url.indexOf('?') ? '&kiosk' : '?kiosk';
-    const url = `${backendUrl}${request.url}${kioskParam}`;
+    
+    const kioskParam = request.url.indexOf('?')!=-1 ? '&kiosk' : '?kiosk';
+    const url = `${backendUrl}${request.url.replace('/d/','d/')}${kioskParam}`;
     console.log(`Trying: ${url}`);
-
-    try {
-      const pdf = await streamPdf(url, { backendUser, backendPass, executablePath ,backendNoLogin,backendPdfSaveLocal,timeoutDuration});
-      response.setHeader('Content-Type', 'application/pdf');
-      pdf.pipe(response);
-    }
-    catch (e) {
-      console.error(e);
+    if (request.url=="/favicon.ico") {
+      // console.error(e);
       response.setHeader('Content-Type', 'text/html');
       response.statusCode = 500;
       response.end('<h1>Internal Server Error 500</h>');
     }
+    else
+    {
+      try {
+        const pdf_path = await streamPdf(url, { backendUser, backendApiKey, backendPass, executablePath ,backendNoLogin,backendPdfSaveLocal,timeoutDuration});
+        response.setHeader('Content-Type', 'application/pdf');
+        const { size } = fs.statSync(pdf_path);
+        const rs = fs.createReadStream(pdf_path);
+        response.setHeader("Content-Length", size);
+        // console.log(pdf);
+        // pdf.pipe(response);
+      /*
+        response.write(pdf);
+        response.end();
+        // pdf.pipe(response);
+        */
+        rs.pipe(response);
+      }
+      catch (e) {
+        console.error(e);
+        response.setHeader('Content-Type', 'text/html');
+        response.statusCode = 500;
+        response.end('<h1>Internal Server Error 500</h>');
+      }
+    }
   }).listen(port, host);
 }
 // console.log(process.env)
-const server = startServer({
+const optionsGrafana={
   port: process.env.GRAFANA_PDF_BIND_PORT || '5555',
   host: process.env.GRAFANA_PDF_BIND_HOST || '::',
   backendUrl: process.env.GRAFANA_PDF_BACKEND_URL ,
+  backendApiKey: process.env.GRAFANA_PDF_BACKEND_API_KEY ,
   backendUser: process.env.GRAFANA_PDF_BACKEND_USER || 'admin',
   backendPass: process.env.GRAFANA_PDF_BACKEND_PASS || 'admin',
   executablePath: process.env.GRAFANA_PDF_CHROME_PATH ,
   timeoutDuration: process.env.GRAFANA_PDF_TIMEOUT_DURATION || '12',
   backendPdfSaveLocal: process.env.GRAFANA_PDF_SAVE_LOCAL || false,
   backendNoLogin: process.env.GRAFANA_PDF_BACKEND_NO_LOGIN || '0'
-});
+}
+function dateFormat(date) {
+  const formatter = new Intl.DateTimeFormat('id', { dateStyle: 'short', timeStyle: 'short'});
+  return formatter.format(date);
+}
+initBrowser(optionsGrafana);
+sleep(3000);//3s
+const server = startServer(optionsGrafana);
 
 server.on('listening', () => {
   const { family, address, port } = server.address();
@@ -177,3 +261,12 @@ server.on('listening', () => {
                                : `http://${address}:${port}`;
   console.log(`Server running at ${url}`);
 });
+
+function sleep(num) {
+  let now = new Date();
+  const stop = now.getTime() + num;
+  while(true) {
+    now = new Date();
+    if(now.getTime() > stop) return;
+  }
+}
